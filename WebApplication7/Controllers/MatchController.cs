6473f6eac59f949659f75ff7ed4658313220636d
GET Academy <terje@getacademy.no>
2022-06-03 14:36:05 +0200

    Initial commit

---
+++ WebApplication7/Controllers/MatchController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class MatchController : Controller
+    {
+        Match[] _matches = new Match[]
+            {
+                new Match{Id=1, HomeTeam="A", AwayTeam="B", Date=DateTime.Now},
+                new Match{Id=2, HomeTeam="A", AwayTeam="C", Date=DateTime.Now.AddDays(-1)},
+                new Match{Id=3, HomeTeam="C", AwayTeam="B", Date=DateTime.Now.AddDays(-2)},
+            };
+
+        public IActionResult List()
+        {
+            return View(_matches);
+        }
+
+        public IActionResult Single(int id)
+        {
+            var match = _matches.FirstOrDefault(m => m.Id == id);
+            return View(match);
+        }
+    }
+}
---
+++ WebApplication7/Models/Match.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication7.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
